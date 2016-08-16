a90fa596f41093d5a8764a36334e9b226b3ccdad
omottec <omottec@gmail.com>
2016-08-16 16:37:46 +0800

    nio test

--- src/v2ch09/io/IoUtils.java
+++ src/v2ch09/io/IoUtils.java
@@ -1,9 +1,9 @@
 package v2ch09.io;
 
-import java.io.DataInput;
-import java.io.DataInputStream;
-import java.io.DataOutput;
-import java.io.IOException;
+import java.io.*;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.zip.CRC32;
 
 /**
  * Created by qinbingbing on 8/10/16.
@@ -45,5 +45,88 @@ public class IoUtils {
         }
         in.skipBytes((size - i) * 2);
         return sb.toString();
+    }
+
+    public static long checksumInputStream(InputStream is) {
+        try {
+            CRC32 crc32 = new CRC32();
+            int b;
+            while ((b = is.read()) != -1)
+                crc32.update(b);
+            return crc32.getValue();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        }
+    }
+
+    public static long checksumFile(String fileName) {
+        FileInputStream fis = null;
+        try {
+            fis = new FileInputStream(fileName);
+            return checksumInputStream(fis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return 0;
+        } finally {
+            close(fis);
+        }
+    }
+
+    public static long bufferedChecksumFile(String fileName) {
+        BufferedInputStream bis = null;
+        try {
+            bis = new BufferedInputStream(new FileInputStream(fileName));
+            return checksumInputStream(bis);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return 0;
+        } finally {
+            close(bis);
+        }
+    }
+
+    public static long checksumRandomAccessFile(String fileName) {
+        RandomAccessFile file = null;
+        try {
+            file = new RandomAccessFile(fileName, "r");
+            long length = file.length();
+            CRC32 crc32 = new CRC32();
+            for (int i = 0; i < length; i++) {
+                file.seek(i);
+                crc32.update(file.readByte());
+            }
+            return crc32.getValue();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        } finally {
+            close(file);
+        }
+    }
+
+    public static long checksumMappedFile(String fileName) {
+        FileChannel channel = null;
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            channel = fis.getChannel();
+            CRC32 crc32 = new CRC32();
+            long size = channel.size();
+            MappedByteBuffer buffer = channel.map(FileChannel.MapMode.READ_ONLY, 0, size);
+            for (int i = 0; i < size; i++)
+                crc32.update(buffer.get(i));
+            return crc32.getValue();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return 0;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return 0;
+        } finally {
+            close(channel);
+        }
     }
 }
---
+++ src/v2ch09/io/NioTest.java
@@ -0,0 +1,52 @@
+package v2ch09.io;
+
+import java.io.File;
+
+/**
+ * Created by qinbingbing on 8/16/16.
+ */
+public class NioTest {
+    public static void main(String[] args) {
+        String fileName = null;
+        if (args.length >= 1)
+            fileName = args[0];
+        else
+            fileName = "." + File.separator + "src"
+                    + File.separator + "v2ch09"
+                    + File.separator + "io"
+                    + File.separator + "NioTest.java";
+        String format = "%s\t%dms\n";
+        System.out.println("InputStream");
+        long start = System.currentTimeMillis();
+        long crcValue = IoUtils.checksumFile(fileName);
+        long end = System.currentTimeMillis();
+        System.out.printf(format, Long.toHexString(crcValue), end - start);
+
+        System.out.println("BufferedInputStream");
+        start = System.currentTimeMillis();
+        crcValue = IoUtils.bufferedChecksumFile(fileName);
+        end = System.currentTimeMillis();
+        System.out.printf(format, Long.toHexString(crcValue), end - start);
+
+        System.out.println("RandomAccessFile");
+        start = System.currentTimeMillis();
+        crcValue = IoUtils.checksumRandomAccessFile(fileName);
+        end = System.currentTimeMillis();
+        System.out.printf(format, Long.toHexString(crcValue), end - start);
+
+        System.out.println("MappedFile");
+        start = System.currentTimeMillis();
+        crcValue = IoUtils.checksumMappedFile(fileName);
+        end = System.currentTimeMillis();
+        System.out.printf(format, Long.toHexString(crcValue), end - start);
+
+        /*InputStream
+        5c61f64d       	48206ms
+                BufferedInputStream
+        5c61f64d       	410ms
+                RandomAccessFile
+        5c61f64d       	73859ms
+                MappedFile
+        5c61f64d       	289ms*/
+    }
+}
