dadb61ba6685e7b3a601b0f790b34984a0bd3a47
omottec <omottec@gmail.com>
2016-08-16 13:35:19 +0800

    file test

---
+++ src/v2ch09/io/FileTest.java
@@ -0,0 +1,35 @@
+package v2ch09.io;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.util.Arrays;
+
+/**
+ * Created by qinbingbing on 8/16/16.
+ */
+public class FileTest {
+    public static void main(String[] args) throws IOException {
+        String dir = "." + File.separator + "src"
+                + File.separator + "v2ch09"
+                + File.separator + "io";
+        File file = new File(dir, "FileTest.java");
+        System.out.println("file.exists():" + file.exists());
+        System.out.println("file.getPath():" + file.getPath());
+        System.out.println("file.getAbsolutePath():" + file.getAbsolutePath());
+        System.out.println("file.getCanonicalPath():" + file.getCanonicalPath());
+        File[] roots = file.listRoots();
+        System.out.println("roots:" + Arrays.toString(roots));
+        System.out.println("file.length():" + file.length());
+        System.out.println("file.getTotalSpace():" + file.getTotalSpace());
+        System.out.println("file.getUsableSpace():" + file.getUsableSpace());
+        System.out.println("file.getFreeSpace():" + file.getFreeSpace());
+        URI uri = file.toURI();
+        System.out.println("uri:" + uri);
+        File tmpFile = File.createTempFile("qbb", ".java");
+        System.out.println("tmpFile.getCanonicalPath():" + tmpFile.getCanonicalPath());
+        File tmpFile1 = File.createTempFile(".pic", "dat", new File("."));
+        System.out.println("tmpFile1.getCanonicalPath():" + tmpFile1.getCanonicalPath());
+        System.out.println("tmpFile1.isHidden():" + tmpFile1.isHidden());;
+    }
+}
---
+++ src/v2ch09/io/FindDirectories.java
@@ -0,0 +1,33 @@
+package v2ch09.io;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Created by qinbingbing on 8/16/16.
+ */
+public class FindDirectories {
+    public static void main(String[] args) {
+        if (args.length == 0)
+            args = new String[] { ".." };
+        File file = new File(args[0]);
+        String[] fileNames = file.list();
+        if (fileNames == null || fileNames.length == 0) {
+            System.out.println("fileNames == null || fileNames.length == 0");
+            return;
+        }
+        File f;
+        try {
+            for (String str : fileNames) {
+                f = new File(file.getPath(), str);
+                if (f.isDirectory()) {
+                    System.out.println(f.getCanonicalPath());
+                    main(new String[] {f.getPath()});
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
