86a2930bf2ff203f1e4f9aec00da3231e441edff
omottec <omottec@gmail.com>
2017-01-10 13:10:03 +0800

    add PairTest2

---
+++ src/v1ch12/PairTest2/Pair.java
@@ -0,0 +1,33 @@
+package v1ch12.PairTest2;
+
+/**
+ * Created by qinbingbing on 1/10/17.
+ */
+public class Pair<T> {
+    private T first;
+    private T second;
+
+    public Pair() {
+    }
+
+    public Pair(T first, T second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public T getSecond() {
+        return second;
+    }
+
+    public void setFirst(T newValue) {
+        first = newValue;
+    }
+
+    public void setSecond(T newValue) {
+        second = newValue;
+    }
+}
---
+++ src/v1ch12/PairTest2/PairTest2.java
@@ -0,0 +1,33 @@
+package v1ch12.PairTest2;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by qinbingbing on 1/10/17.
+ */
+public class PairTest2 {
+    public static void main(String[] args) {
+        GregorianCalendar[] birthdays = {
+                new GregorianCalendar(1906, Calendar.DECEMBER, 9),
+                new GregorianCalendar(1815, Calendar.DECEMBER, 10),
+                new GregorianCalendar(1903, Calendar.DECEMBER, 3),
+                new GregorianCalendar(1910, Calendar.JUNE, 22)
+        };
+        Pair<GregorianCalendar> mm = ArrayAlg.minmax(birthdays);
+        System.out.println("min=" + mm.getFirst());
+        System.out.println("max=" + mm.getSecond());
+    }
+}
+
+class ArrayAlg {
+    public static <T extends Comparable<? super T>> Pair<T> minmax(T[] a) {
+        if (a == null || a.length == 0) return null;
+        T min = a[0], max = a[0];
+        for (int i = 1; i < a.length; i++) {
+            if (min.compareTo(a[i]) > 0) min = a[i];
+            if (max.compareTo(a[i]) < 0) max = a[i];
+        }
+        return new Pair<T>(min, max);
+    }
+}
